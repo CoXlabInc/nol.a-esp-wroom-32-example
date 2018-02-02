8fc7a3cae5aa3037cd3987b0113ef7e56e7feebf
Jongsoo Jeong <jsjeong@coxlab.kr>
2018-02-02 15:20:10 +0900

    Added an example.

---
+++ Basic/main.cpp
@@ -0,0 +1,14 @@
+#include <cox.h>
+
+//Timer timerHello;
+
+// static void taskHello(void *) {
+//
+// }
+
+void setup() {
+  printf("hello World!\n");
+  // Serial.begin(115200);
+  // timerHello.onFired(taskHello, NULL);
+  // timerHello.startPeriodic(1000);
+}
