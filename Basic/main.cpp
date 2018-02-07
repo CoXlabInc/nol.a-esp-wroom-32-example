6f08573121675c5f250d2323c7d6b0ceb6cbfdb5
Jongsoo Jeong <jsjeong@coxlab.kr>
2018-02-07 11:24:16 +0900

    CS-261 Updated example.

--- Basic/main.cpp
+++ Basic/main.cpp
@@ -1,14 +1,34 @@
 #include <cox.h>
 
-//Timer timerHello;
+// Timer timerHello;
 
-// static void taskHello(void *) {
-//
-// }
+static void taskHello(void *) {
+  Serial.println("Hello! This is Serial.");
+  Serial2.println("Hello! This is Serial2.");
+  Serial.printf("Delay 5 sec...\n");
+  delay(5000);
+  postTask(taskHello, NULL);
+}
+
+static void eventSerialRx(SerialPort &p) {
+  while (p.available() > 0) {
+    char c = p.read();
+    p.write(c); //echo
+  }
+}
 
 void setup() {
-  printf("hello World!\n");
-  // Serial.begin(115200);
+  Serial.begin(115200);
+  Serial.printf("*** [ESP-WROOM-32] Basic ***\n");
+  Serial.onReceive(eventSerialRx);
+  Serial.listen();
+
+  Serial2.begin(115200);
+  Serial2.printf("*** [ESP-WROOM-32] Basic ***\n");
+  Serial2.onReceive(eventSerialRx);
+  Serial2.listen();
+
   // timerHello.onFired(taskHello, NULL);
   // timerHello.startPeriodic(1000);
+  postTask(taskHello, NULL);
 }
