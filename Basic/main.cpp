fe0d16105c4bccf523d9804e909c2535d1279022
Jongsoo Jeong <jsjeong@coxlab.kr>
2018-03-04 01:45:05 +0900

    CS-261 Added GPIO test codes.

--- Basic/main.cpp
+++ Basic/main.cpp
@@ -1,13 +1,35 @@
 #include <cox.h>
 
 Timer timerHello;
+Timer timerHello2;
 
 static void taskHello(void *) {
   Serial.printf(
-    "Hello! This is Serial. msec:%u, usec:%u, sec:%u\n",
+    "Hello every 1sec. msec:%u, usec:%u, sec:%u,",
     millis(), micros(), seconds()
   );
-  Serial2.printf("Hello! This is Serial2.\n");
+  Serial2.printf(
+    "Hello every 1sec. This is Serial. msec:%u, usec:%u, sec:%u\n",
+    millis(), micros(), seconds()
+  );
+
+  digitalToggle(2);
+  Serial.printf(" GPIO0:%d, GPIO2:%d\n", digitalRead(0), digitalRead(2));
+}
+
+static void taskHello2(void *) {
+  Serial.printf(
+    "Hello every 5sec. msec:%u, usec:%u, sec:%u\n",
+    millis(), micros(), seconds()
+  );
+  Serial2.printf(
+    "Hello every 5sec. msec:%u, usec:%u, sec:%u\n",
+    millis(), micros(), seconds()
+  );
+}
+
+static void eventKeyPressed() {
+  Serial.printf("* Key connected to GPIO0 is pressed!\n");
 }
 
 static void eventSerialRx(SerialPort &p) {
@@ -19,15 +41,24 @@ static void eventSerialRx(SerialPort &p) {
 
 void setup() {
   Serial.begin(115200);
-  Serial.printf("*** [ESP-WROOM-32] Basic ***\n");
+  Serial.println("*** [ESP-WROOM-32] Basic ***");
   Serial.onReceive(eventSerialRx);
   Serial.listen();
 
   Serial2.begin(115200);
-  Serial2.printf("*** [ESP-WROOM-32] Basic ***\n");
+  Serial2.println("*** [ESP-WROOM-32] Basic ***");
   Serial2.onReceive(eventSerialRx);
   Serial2.listen();
 
   timerHello.onFired(taskHello, NULL);
-  timerHello.startPeriodicMicros(1000000);
+  timerHello.startPeriodic(1000);
+
+  timerHello2.onFired(taskHello2, NULL);
+  timerHello2.startPeriodic(5000);
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+
+  pinMode(0, INPUT);
+  attachInterrupt(0, eventKeyPressed, FALLING);
 }
