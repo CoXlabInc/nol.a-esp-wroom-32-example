5f4ed23d293557395c4e3a92df5c0982b49cdbcc
Jongsoo Jeong <jsjeong@coxlab.kr>
2018-10-02 10:55:32 +0900

    Fixed to use BLEAdvertisedDevice's methods.

--- BLEScanner/main.cpp
+++ BLEScanner/main.cpp
@@ -4,6 +4,49 @@
 BLEScan* pBLEScan = nullptr;
 bool isScanning = false;
 
+class MyAdvertisedDeviceListener: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    BLEAddress addr = advertisedDevice.getAddress();
+    Serial.printf("[SCANNED] Source Address: %s", addr.toString().c_str());
+
+    if (advertisedDevice.haveName()) {
+      Serial.printf(", Name: %s", advertisedDevice.getName().c_str());
+    }
+
+    if (advertisedDevice.haveAppearance()) {
+      Serial.printf(", Appearance: 0x%04X", advertisedDevice.getAppearance());
+    }
+
+    if (advertisedDevice.haveManufacturerData()) {
+      Serial.print(", Manufacturer data:");
+      std::string manufacturerData = advertisedDevice.getManufacturerData();
+      for (uint8_t i = 0; i < manufacturerData.length(); i++) {
+        Serial.printf(" %02X", (uint8_t) manufacturerData.data()[i]);
+      }
+    }
+
+    if (advertisedDevice.haveServiceUUID()) {
+      BLEUUID serviceUUID = advertisedDevice.getServiceUUID();
+      Serial.printf(", service UUID: %s", serviceUUID.toString().c_str());
+    }
+
+    if (advertisedDevice.haveTXPower()) {
+      int8_t txPower = advertisedDevice.getTXPower();
+      Serial.printf(", TX power:%d dBm", txPower);
+    }
+
+    if (advertisedDevice.haveRSSI()) {
+      int rssi = advertisedDevice.getRSSI();
+      Serial.printf(", RSSI: %d dB", rssi);
+    }
+
+    Serial.println();
+  }
+};
+
+
+MyAdvertisedDeviceListener listener;
+
 static void eventButtonPressed() {
   if (isScanning) {
     Serial.println("* Turn off scanner!");
@@ -19,12 +62,6 @@ static void eventButtonPressed() {
   }
 }
 
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-  void onResult(BLEAdvertisedDevice advertisedDevice) {
-    Serial.printf("Advertised Device: %s (RSSI:%d dB)\n", advertisedDevice.toString().c_str(), advertisedDevice.getRSSI());
-  }
-};
-
 void setup() {
   Serial.begin(115200);
   printf("\n*** [ESP-WROOM-32] BLE Scanner ***\n");
@@ -34,7 +71,7 @@ void setup() {
 
   BLEDevice::init("");
   pBLEScan = BLEDevice::getScan(); //create new scan
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks(), true);
+  pBLEScan->setAdvertisedDeviceCallbacks(&listener, true);
   pBLEScan->activeScan = true;
   isScanning = pBLEScan->start(0, nullptr);
   if (isScanning) {
